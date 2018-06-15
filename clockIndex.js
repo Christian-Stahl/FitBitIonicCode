347f36c93461c6313746728dc40ee374cde1f21d
Christian Stahl <chstahl@outlook.com>
2018-06-15 18:49:51 +0200

    Add files via upload

--- clockIndex.js
+++ clockIndex.js
@@ -52,6 +52,70 @@ if (difference > 5) {
 
 
 
+// dagens datum
+//-----------------------------------------
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1; //January is 0!
+let yyyy = today.getFullYear();
+  if(dd < 10) {
+      dd = '0' + dd
+  } 
+  if(mm < 10) {
+      mm = '0' + mm
+  } 
+let today = yyyy + '-' + mm + '-' + dd;
+console.log("dagens datum: " + today );
+
+
+
+// Visa batterinivån
+//-----------------------------------------
+import { battery } from "power";
+
+const myBattery = document.getElementById("myBattery");
+myBattery.text = `Batteri ${battery.chargeLevel}%`; // initialize on startup
+battery.onchange = (charger, evt) => {
+   myBattery.text = `${battery.chargeLevel}%`;
+}
+
+
+// Spinner
+//-----------------------------------------
+let spinner = document.getElementById("spinner");
+  spinner.state = "enabled"; // Start
+  setTimeout(function() {
+    spinner.state = "disabled";  
+    // Lägg in vad som ska hända här när spinnern är stoppad, efter 2 sek i detta fall
+  }, 2000)
+
+
+// Kolla veckodag, i detta exempel i en ticker funktion
+//-----------------------------------------
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  let weekDayNr = today.getDay(); // Kolla veckodagsnummer
+   
+     /* Uppmuntra om fre, lö el sön */
+     if (weekDayNr == 5) {
+        let weekDay = "yey, fredag i dag!"
+         weekDayMsg.text = `${weekDay}`;
+     }
+     else if (weekDayNr == 6) {
+        let weekDay = "Bästa dagen, lördag!"
+         weekDayMsg.text = `${weekDay}`;
+     }
+     else if (weekDayNr == 7) {
+        let weekDay = "Härliga söndag!"
+         weekDayMsg.text = `${weekDay}`;
+     }
+     else {
+     }
+
+
+  
+}
 
 
 
